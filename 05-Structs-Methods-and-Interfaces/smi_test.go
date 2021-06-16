562de22734f881ababb4cd8bbd724c4e5f670b4c
petros <pit.trak@gmail.com>
2021-06-16 15:18:49 +0300

    Testing with interfaces

--- 05-Structs-Methods-and-Interfaces/smi_test.go
+++ 05-Structs-Methods-and-Interfaces/smi_test.go
@@ -4,24 +4,22 @@ import "testing"
 
 func TestArea(t *testing.T) {
 
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g, but want %g", got, want)
+		}
+	}
+
 	t.Run("Rectangles", func(t *testing.T) {
 		rect := Rectangle{12.0, 6.0}
-		got := rect.Area()
-		want := 72.0
-
-		if got != want {
-			t.Errorf("got %.2f, but want %.2f", got, want)
-		}
+		checkArea(t, rect, 72.0)
 	})
 
 	t.Run("Circles", func(t *testing.T) {
 		circle := Circle{10.0}
-		got := circle.Area()
-		want := 31.41592653589793
-
-		if got != want {
-			t.Errorf("got %.2f, but want %.2f", got, want)
-		}
+		checkArea(t, circle, 31.41592653589793)
 	})
 
 }
