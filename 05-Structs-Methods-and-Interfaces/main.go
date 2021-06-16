5e0ea2622d0b7516fc0ad450a3853c46270d8d2f
petros <pit.trak@gmail.com>
2021-06-16 17:33:52 +0300

    Testing with Structs, Methods and Interfaces

--- 05-Structs-Methods-and-Interfaces/main.go
+++ 05-Structs-Methods-and-Interfaces/main.go
@@ -13,6 +13,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 type Shape interface {
 	Area() float64
 }
@@ -27,6 +32,10 @@ func (r Rectangle) Area() float64 {
 
 func (c Circle) Area() float64 {
 	return c.Radius * math.Pi
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
 }
 
 func main() {
--- 05-Structs-Methods-and-Interfaces/smi_test.go
+++ 05-Structs-Methods-and-Interfaces/smi_test.go
@@ -5,19 +5,26 @@ import "testing"
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 31.41592653589793},
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
-	for _, test := range areaTests {
-		got := test.shape.Area()
-		if got != test.want {
-			t.Errorf("got %g, but want %g", got, test.want)
-		}
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+
 	}
+
 }
 
 // func TestArea(t *testing.T) {
