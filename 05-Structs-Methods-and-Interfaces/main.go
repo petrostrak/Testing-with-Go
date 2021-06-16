89843c09367f58ef0eaaaae8eba819a7f16fe3d0
petros <pit.trak@gmail.com>
2021-06-16 14:52:53 +0300

    Testing with Methods

--- 05-Structs-Methods-and-Interfaces/main.go
+++ 05-Structs-Methods-and-Interfaces/main.go
@@ -1,15 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"math"
+)
 
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
-
-const (
-	pi = 3.14
-)
 
 type Circle struct {
 	Radius float64
@@ -19,16 +17,14 @@ func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
 
-func CircleArea(circle Circle) float64 {
-	return circle.Radius * pi
+func (c Circle) Area() float64 {
+	return c.Radius * math.Pi
 }
 
 func main() {
-	fmt.Println(Perimeter(Rectangle{Width: 10.0, Height: 10.0}))
-	fmt.Println(Area(Rectangle{Width: 12.0, Height: 6.0}))
-	fmt.Println(CircleArea(Circle{Radius: 10.0}))
+
 }
--- 05-Structs-Methods-and-Interfaces/smi_test.go
+++ 05-Structs-Methods-and-Interfaces/smi_test.go
@@ -6,7 +6,7 @@ func TestArea(t *testing.T) {
 
 	t.Run("Rectangles", func(t *testing.T) {
 		rect := Rectangle{12.0, 6.0}
-		got := Area(rect)
+		got := rect.Area()
 		want := 72.0
 
 		if got != want {
@@ -16,8 +16,8 @@ func TestArea(t *testing.T) {
 
 	t.Run("Circles", func(t *testing.T) {
 		circle := Circle{10.0}
-		got := CircleArea(circle)
-		want := 31.400000000000002
+		got := circle.Area()
+		want := 31.41592653589793
 
 		if got != want {
 			t.Errorf("got %.2f, but want %.2f", got, want)
