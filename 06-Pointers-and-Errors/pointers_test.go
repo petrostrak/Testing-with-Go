1ee4e65e5fe6ce70bdf9cfeb235efb89cfb622cf
petros <pit.trak@gmail.com>
2021-06-18 12:27:47 +0300

    Testing with pointers and errors

--- 06-Pointers-and-Errors/pointers_test.go
+++ 06-Pointers-and-Errors/pointers_test.go
@@ -13,15 +13,45 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
+	assertError := func(t testing.TB, got error, want string) {
+		t.Helper()
+		if got == nil {
+			t.Fatal("didn't get an error but wanted one")
+		}
+
+		if got.Error() != want {
+			t.Errorf("got %q, but want %q", got, want)
+		}
+
+	}
+
+	assertNoError := func(t testing.TB, got error) {
+		t.Helper()
+		if got != nil {
+			t.Fatal("got an error but didn't want one")
+		}
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
-	t.Run("Withdraw", func(t *testing.T) {
+	t.Run("Withdraw with funds", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+		assertNoError(t, err)
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(Bitcoin(10))
+
+		err := wallet.Withdraw(Bitcoin(100))
+
 		assertBalance(t, wallet, Bitcoin(10))
+		assertError(t, err, ErrInsufficientFunds.Error())
 	})
 }
