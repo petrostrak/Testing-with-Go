a0f179254655909781a96633defe58e1ae43075e
petros <pit.trak@gmail.com>
2021-06-18 12:10:59 +0300

    Testing Pointers and errors

--- 06-Pointers-and-Errors/pointers_test.go
+++ 06-Pointers-and-Errors/pointers_test.go
@@ -3,14 +3,25 @@ package main
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
 
-	got := wallet.Balance()
-	want := 10
-
-	if got != want {
-		t.Errorf("got %d, but want %d", got, want)
+		if got != want {
+			t.Errorf("got %s, but want %s", got, want)
+		}
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
 }
