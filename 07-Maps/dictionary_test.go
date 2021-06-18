9c19d46daaf708870af2132686f4f86c797e975f
petros <pit.trak@gmail.com>
2021-06-18 17:56:10 +0300

    Testing maps

--- 07-Maps/dictionary_test.go
+++ 07-Maps/dictionary_test.go
@@ -18,6 +18,19 @@ func assertError(t testing.TB, got, want error) {
 	}
 }
 
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if definition != got {
+		t.Errorf("got %q, but want %q", got, definition)
+	}
+}
+
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 	t.Run("known word", func(t *testing.T) {
@@ -32,5 +45,13 @@ func TestSearch(t *testing.T) {
 
 		assertError(t, got, ErrWordNotFound)
 	})
+
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	word := "test"
+	definition := "this is just a test"
+	dictionary.Add(word, definition)
 
 }
--- 07-Maps/main.go
+++ 07-Maps/main.go
@@ -19,7 +19,12 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
+}
+
 func main() {
-	d := Dictionary{"test": "This is just a test"}
+	d := Dictionary{}
+	d.Add("test", "This is just a test")
 	fmt.Println(d.Search("test"))
 }
