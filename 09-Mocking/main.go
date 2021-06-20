21bca247b2fa81b948a955db86b28c77e0b24d12
petros <pit.trak@gmail.com>
2021-06-21 01:09:19 +0300

    Testing with mocking

---
+++ 09-Mocking/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const (
+	finalWord      = "Go!\n"
+	countdownStart = 3
+)
+
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
+		fmt.Fprintln(out, i)
+	}
+
+	time.Sleep(1 * time.Second)
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	Countdown(os.Stdout)
+}
